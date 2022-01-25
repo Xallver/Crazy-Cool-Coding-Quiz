8f223a248a485e45a0277de0fb5598722807c637
Xallver <jamesstanley11@gmail.com>
2022-01-25 14:07:11 -0500

    added questions to JS

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+// Quiz Questions
+const questions = [
+    {
+        question: "What is the first index of an array?",
+        choices: ["a. 1", "b. 0", "c. 2", "d. 3"],
+        answer: "b. 0"
+    },
+    {
+        question: "How do you create a JavaScript function?",
+        choices: ["a. function:myFunction()", "b. function = myFunction()", "c. createFunction()", "d. function myFunction()"],
+        answer: "d. function myFunction()"
+    },
+    {
+        question: "In which HTML element do we put the JavaScript?",
+        choices: ["a. <javascript>", "b. <js>", "c. <script>", "d. <jsscript>"],
+        answer: "c. <script>"
+    },
+    {
+        question: "Which one of these is NOT used as a Javascript Data Type?",
+        choices: ["a. number", "b. string", "c. boolean", "d. alerts"],
+        answer: "d. alerts"
+    },
+    {
+        question: "How you do call a function named thisFunction()?",
+        choices: ["a. call thisFunction", "b. thisFunction()", "c. call thisFunction()", "d. call function thisFunction()"],
+        answer: "b. thisFunction()"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ["a. alertBox(\"Hello World\");", "b. alert(\"Hello World\");", "c. msgBox(\"Hello World\")", "d. msg(\"Hello World\");" ],
+        answer: "b. alert('Hello World');"
+    },
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        choices: ["a. if (i == 5)", "b. if i = 5", "c. if i = 5 then", "d. if i == 5 then"],
+        answer: "a. if (i == 5)"
+    },
+    {
+        question: "How does a FOR loop start?",
+        choices: ["a. for i = 1 to 5", "b. for(i = 0; i <= 5)", "c. for(i <= 5; i++)", "d. for (i = 0; i <= 5; i++)"],
+        answer: "d. for (i = 0; i <= 5; i++)"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: ["a. 'this is a comment", "b. //This is a comment", "c. <!--This is a comment-->"],
+        answer: "b. //This is a comment"
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        choices: ["a. var colors = [\"red","green","blue\"]", "b. var colors = (1:\"red\", 2:\"green\", 3:\"blue\")", "c. var colors = \"red\", \"green\", \"blue\"", "d. var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")"],
+        answer: "a. var colors = [\"red\", \"green\", \"blue\"]"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["a. Math.round(7.25)", "b. Math.rnd(7.25)", "c. rnd(7.25)", "d. round(7.25)"],
+        answer: "a. Math.round(7.25)"
+    },
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ___.",
+        choices: ["a. =", "b. !=", "c. ==", "d. equals"],
+        answer: "c. =="
+    }
+]
