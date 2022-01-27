a861a92e7867c926688792c1869a5d6ed4dc337d
Xallver <jamesstanley11@gmail.com>
2022-01-26 20:11:50 -0500

    connected elements from HTML page to JS page using variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,3 +64,40 @@ const questions = [
 // End of Questions
 
 // Connecting Elements
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+var questionDiv = document.getElementById("questionDiv");
+var questionTitle = document.getElementById("questionTitle");
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+var answerCheck = document.getElementById("answerCheck");
+
+var summary = document.getElementById("summary");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var initialInput = document.getElementById("initialInput");
+var everything = document.getElementById("everything");
+
+var highScoreSection = document.getElementById("highScoreSection");
+var finalScore = document.getElementById("finalScore");
+
+var goBackBtn = document.getElementById("goBackBtn");
+var clearHighScoreBtn = document.getElementById("clearHighScoreBtn");
+var viewHighScore = document.getElementById("viewHighScore");
+var listOfHighScores = document.getElementById("listOfHighScores");
+// End of Connecting Elements
+
+// Random Variables
+var questionNum = 0;
+var correctAns = 0;
+var questionIndex = 0;
+var scoreResult;
+// End of Random Variables
+
+// When user click's start button, timer will start
